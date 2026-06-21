560962dde4daa75fa4c44fe65e14682011ce20d1
Pak Wai <virtue.sign@gmail.com>
2026-06-21 11:30:27 +0800

    debug: add console.log to debug origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -29,6 +29,11 @@ const trustedOrigins = isProd
     ]
   : ['*']
 
+console.log('isProd', isProd);
+console.log('trustedOrigins', trustedOrigins);
+console.log('previewURL', previewURL);
+console.log('runtimeURL', runtimeURL);
+
 export const auth = betterAuth({
   database: pool,
   baseURL,
